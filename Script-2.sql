746ba6cd4026041edf2c8d16a9a8b5a820deeea4
DimaLukyanov <101321434+DimaLukyanov@users.noreply.github.com>
2023-05-19 19:13:08 +0300

    Delete Script-2.sql

--- Script-2.sql
+++
@@ -1,9 +0,0 @@
---Показать поля ticket_no, flight_id и boarding_no из 
---Представления boarding_passes, 
---где номер места ниже 5000;
-SELECT 
-      bp.ticket_no,
-      bp.flight_id,
-      bp."boarding_no"
-FROM demo.bookings.boarding_passes AS bp 
-WHERE "boarding_no" <= 5000;
