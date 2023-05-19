7c8c56e174a962c69231b42de903e1d0082a5ce5
DimaLukyanov <101321434+DimaLukyanov@users.noreply.github.com>
2023-05-19 19:13:53 +0300

    Delete Script-6.sql

--- Script-6.sql
+++
@@ -1,7 +0,0 @@
---Создать столбец "NULL" так, чтобы получить значение NULL,
---если время продолжительности полёта 05:50:00 из Представления Routes.
-
-SELECT 
-      r.duration 
-      ,NULLIF(duration , '05:50:00') AS "NULL"
-FROM demo.bookings.routes AS r
