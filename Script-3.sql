f917acacd4b45990b55359b119990d90319d4b80
DimaLukyanov <101321434+DimaLukyanov@users.noreply.github.com>
2023-05-19 19:13:19 +0300

    Delete Script-3.sql

--- Script-3.sql
+++
@@ -1,8 +0,0 @@
---Показать поля airport_name и city из 
---Представления Airports, 
---где города в названии содержат 10 символов.
-SELECT 
-      a.airport_name 
-    , a.city 
-FROM demo.bookings.airports AS a 
-WHERE city LIKE '__________';
